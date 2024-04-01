c05a1afb329046e21526f08d232cc0a1d37263c9
Dagaet <alojera15@gmail.com>
2024-04-01 12:07:12 +0100

    Test(Red): PasswordValidator should Have at least a lower case letter

--- src/test/kotlin/dev/kata/passwordValidator/PasswordValidatorShould.kt
+++ src/test/kotlin/dev/kata/passwordValidator/PasswordValidatorShould.kt
@@ -9,12 +9,19 @@ internal class PasswordValidatorShould {
     @Test
     fun `Have at least 8 characters`() {
         val passwordValidator = PasswordValidator()
-        val isPasswordValid = passwordValidator.execute("1234567A")
+        val isPasswordValid = passwordValidator.execute("123456Ab")
         assertThat(isPasswordValid).isEqualTo(true)
     }
 
     @Test
     fun `Have at least a capital letter`() {
+        val passwordValidator = PasswordValidator()
+        val isPasswordValid = passwordValidator.execute("123456bC")
+        assertThat(isPasswordValid).isEqualTo(true)
+    }
+
+    @Test
+    fun `Have at least a lower case letter`() {
         val passwordValidator = PasswordValidator()
         val isPasswordValid = passwordValidator.execute("123456bC")
         assertThat(isPasswordValid).isEqualTo(true)
