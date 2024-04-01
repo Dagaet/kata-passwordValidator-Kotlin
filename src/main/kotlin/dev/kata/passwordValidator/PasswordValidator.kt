b9590c7485e9d395a1247199415b2e6c1e9e046b
Dagaet <alojera15@gmail.com>
2024-04-01 11:06:49 +0100

    Test(Green)(Refactor): PasswordValidator should Have at least 8 characters

--- src/main/kotlin/dev/kata/passwordValidator/PasswordValidator.kt
+++ src/main/kotlin/dev/kata/passwordValidator/PasswordValidator.kt
@@ -4,9 +4,6 @@ class PasswordValidator{
     fun execute(something: String): Boolean {
         val predicate: (Char) -> Boolean = { it.isUpperCase() }
         val mayus = something.find(predicate)
-        if (something.length >= 8 && mayus.toString().length == 1){
-            return true
-        }
-        return false
+        return something.length >= 8 && mayus.toString().length == 1
     }
 }
