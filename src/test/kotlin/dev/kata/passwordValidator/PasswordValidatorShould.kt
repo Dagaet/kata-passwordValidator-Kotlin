ec6a3f91829f7e4f41d9893f163efe56e4b1f271
Dagaet <alojera15@gmail.com>
2024-04-01 11:02:56 +0100

    Test(Red): PasswordValidator should Have at least 8 characters

--- src/test/kotlin/dev/kata/passwordValidator/PasswordValidatorShould.kt
+++ src/test/kotlin/dev/kata/passwordValidator/PasswordValidatorShould.kt
@@ -9,7 +9,14 @@ internal class PasswordValidatorShould {
     @Test
     fun `Have at least 8 characters`() {
         val passwordValidator = PasswordValidator()
-        val isPasswordValid = passwordValidator.execute("12345678")
+        val isPasswordValid = passwordValidator.execute("1234567A")
+        assertThat(isPasswordValid).isEqualTo(true)
+    }
+
+    @Test
+    fun `Have at least a capital letter`() {
+        val passwordValidator = PasswordValidator()
+        val isPasswordValid = passwordValidator.execute("123456bC")
         assertThat(isPasswordValid).isEqualTo(true)
     }
 }
